241248357f6f6d72ff6de0f374a7bb8d254cae54
Francisco Favela <ffavela@gmail.com>
2019-03-07 16:54:09 -0600

    added code

---
+++ README.md
@@ -0,0 +1,56 @@
+The program "damfits" is a simple python program for doing simple
+operations of fits files as they are handled in the damic project.
+
+Please see INSTALL for prerequisites and installation instructions.
+
+Examples:
+
+Simple invocation:
+
+$ damfits
+usage: damfits[options] afile.fits# use -h for help
+
+Getting a little help:
+
+$ damfits -h
+damfits [-h|--help]
+
+damfits file.fits #displays fits file info
+
+damfits -p [extraOptions] file.fits #plots
+
+extraOptions:
+
+	-i:	Needs one integer as argument
+ 		so it can select the corresponding image
+
+	-b:	Needs one integer for defining the bining
+
+	--side:	Needs one argument specifying the left or right
+		side of the image
+
+	--noLog:	A simple flag for turning of the ylog scale
+
+	--color:	Needs one argument defining the color
+
+For seeing some info about the fits file:
+
+$ damfits d44_snolab_Int-200_Exp-100000_4283.fits
+Filename: d44_snolab_Int-200_Exp-100000_4283.fits
+No.    Name      Ver    Type      Cards   Dimensions   Format
+  0  PRIMARY       1 PrimaryHDU     105   ()
+  1                1 ImageHDU       110   (8544, 4298)   int16 (rescales to uint16)
+  2                1 ImageHDU       110   (8544, 4298)   int16 (rescales to uint16)
+  3                1 ImageHDU       110   (8544, 4298)   int16 (rescales to uint16)
+  4                1 ImageHDU       110   (8544, 4298)   int16 (rescales to uint16)
+  5                1 ImageHDU       110   (8544, 4298)   int16 (rescales to uint16)
+  6                1 ImageHDU       110   (8544, 4298)   int16 (rescales to uint16)
+  7                1 ImageHDU       110   (8544, 4298)   int16 (rescales to uint16)
+  8                1 ImageHDU       110   (8544, 4298)   int16 (rescales to uint16)
+  9                1 ImageHDU       110   (8544, 4298)   int16 (rescales to uint16)
+ 10                1 ImageHDU       110   (8544, 4298)   int16 (rescales to uint16)
+ 11                1 ImageHDU       110   (8544, 4298)   int16 (rescales to uint16)
+ 12                1 ImageHDU       110   (8544, 4298)   int16 (rescales to uint16)
+
+
+Try it out and post suggestions and comments.
---
+++ damfits.py
@@ -0,0 +1,212 @@
+#!/usr/bin/python
+
+import sys
+import os
+from os.path import basename #For printing nicely argv[0]
+import os.path
+
+import matplotlib.pyplot as plt
+from astropy.io import fits
+
+accOpts=['-h', '--help', '-p',\
+         '-i', '-b', '--side', '--noLog',\
+         '--color']
+
+def createExtraOptionsDict(accOpts):
+    extrOptDict={}
+    for e in accOpts[3:]:
+        extrOptDict[e]=""
+
+    extrOptDict['-i']="""Needs one integer as argument
+ \t\tso it can select the corresponding image"""
+    extrOptDict['-b']="""Needs one integer for defining the bining"""
+    extrOptDict['--side']="""Needs one argument specifying the left or right
+\t\tside of the image"""
+    extrOptDict['--noLog']="""A simple flag for turning of the ylog scale"""
+    extrOptDict['--color']="""Needs one argument defining the color"""
+    return extrOptDict
+
+extrOptDict=createExtraOptionsDict(accOpts)
+
+def getMyOptDict(myArgs):
+    myOptDict={}
+
+    tmpOpt=''
+    for i in range(len(myArgs)):
+        e=myArgs[i]
+        if e[0] == '-':
+            myOptDict[e]=[]
+            tmpOpt=e
+            continue #Just skipping the option
+
+        if tmpOpt != '':
+            myOptDict[tmpOpt].append(i)
+
+    return myOptDict
+
+def handleSubCases(hdu_list, myOptDict, argv):
+    iNum=1 #Which image to plot
+    bining=300 #default bining
+    logB=True #log y
+    side="right" #or left of the image
+    color="blue" #for the histogram
+
+    if "-b" in myOptDict:
+        bining=argv[myOptDict["-b"][0]]
+        if not bining.isdigit():
+            print("error: bining needs a positive integer")
+            return False
+        bining=int(bining)
+        if bining <= 0:
+            print("error: bining needs a positive integer")
+            return False
+
+    if "--side" in myOptDict:
+        side=argv[myOptDict["--side"][0]]
+        if side == "l" or side == "left":
+            side="left"
+        elif side =="r" or side == "right":
+            side="right"
+        else:
+            print("error: %s is an invalid side" %(side))
+            print("valid sides are left or l and right or r")
+            return False
+
+    if "--noLog" in myOptDict:
+        logB=False
+
+    if "-i" in myOptDict:
+        #The image number
+        iNumStr=argv[myOptDict["-i"][0]]
+        if not iNumStr.isdigit():
+            print("error: %s should be a positive integer")
+        iNum=int(iNumStr)
+        checkBool,totNum=checkIfValidFitsImgNum(hdu_list,iNum)
+        if not checkBool:
+            print("error:  1 <= %s < %d should be satisfied" %(iNum,totNum))
+            return False
+
+        if '--color' in myOptDict:
+            color=argv[myOptDict['--color'][0]]
+
+    plotHistoData(hdu_list,iNum,bining,logB,side,color)
+    return True
+
+def printHelp(argv):
+    print("%s [-h|--help]\n" %(basename(argv[0])))
+    print("%s file.fits #displays fits file info\n" %(basename(argv[0])))
+    print("%s -p [extraOptions] file.fits #plots \n" %(basename(argv[0])))
+    print("extraOptions:\n")
+    for e in extrOptDict:
+        print("\t%s:\t%s\n" %(e,extrOptDict[e]))
+
+
+def myXFill(imageStuff,side):
+    halfXVal=len(imageStuff[0])//2 #// is integer division
+    x=[]
+    if side == 'right':
+        for myRow in imageStuff:
+            x+=list(myRow[:halfXVal+1]) #the right side
+    else:
+        for myRow in imageStuff:
+            x+=list(myRow[halfXVal+1:]) #the left side
+
+    return x
+
+def plotHistoData(hdu_list,iNum=1,bining=300,\
+                 logB=True,side="right",\
+                 color="blue"):
+
+    halfXVal=len(hdu_list[iNum].data[0])//2 #// is integer division
+    imageStuff=hdu_list[iNum].data
+    x=myXFill(imageStuff,side)
+    n,bins,patches = plt.hist(x,bining,facecolor=color,alpha=0.5)
+    if logB:
+        plt.yscale('log', nonposy='clip')
+    plt.show()
+
+def openfits(b):
+    hdu_list = fits.open(b)
+    hdu_list.info()
+    print(hdu_list[0])
+    print(hdu_list[1])
+    print(hdu_list[1].data[0])
+    print(len(hdu_list[1].data))
+    print(len(hdu_list[1].data[0]))
+
+    i=3
+    print(i)
+    imageStuff=hdu_list[i].data
+    print("half x value = ")
+    halfXVal=len(hdu_list[i].data[0])//2
+    print(halfXVal)
+    # return
+    x=[]
+    for  myRow in imageStuff:
+        # print("len(myRow) = %d " % (len(myRow)))
+        # x+=list(myRow[halfXVal+1:]) #the left side
+        x+=list(myRow[:halfXVal+1]) #the right side
+
+    # plt.imshow(hdu_list[1], cmap='gray')
+    # plt.colorbar()
+
+    num_bins = 300
+    n,bins,patches = plt.hist(x, num_bins, facecolor='blue', alpha=0.5)
+    plt.yscale('log', nonposy='clip')
+    plt.show()
+
+
+def checkIfValidOpts(myOptDict, accOpts):
+    for e in myOptDict:
+        if e not in accOpts:
+            print("error: %s is not a valid option" %(e))
+            return False
+    return True
+
+def getNumOfFitsImgs(hdu_list):
+    return len(hdu_list)
+
+def checkIfValidFitsImgNum(hdu_list,iNum):
+    totNum=getNumOfFitsImgs(hdu_list)
+    if 0< iNum < totNum:
+        return True,totNum
+    return False,totNum
+
+def main(argv):
+    if len(argv) < 2:
+        print("usage: "\
+              +basename(argv[0])+\
+              "[options] afile.fits# use -h for help")
+        return 1
+    myOptDict=getMyOptDict(argv)
+
+    if not checkIfValidOpts(myOptDict, accOpts):
+        return 4
+
+    if '-h' in myOptDict or '--help' in myOptDict:
+        printHelp(argv)
+        return 5
+
+    # for e in myOptDict:
+    #     print(e, myOptDict[e])
+
+    myFitsF=argv[-1]
+    if not os.path.isfile(myFitsF):
+        print("error: "+myFitsF+" is not a file")
+        return 2
+    # openfits("d44_snolab_Int-200_Exp-100000_4283.fits")
+
+    try:
+        hdu_list = fits.open(myFitsF)
+    except:
+        print("error: "+myFitsF+" is not a valid fits file.")
+        return 3
+
+    # openfits(myFitsF)
+    if '-p' not in myOptDict:
+        hdu_list.info()
+    else:
+        handleSubCases(hdu_list, myOptDict, argv)
+
+if __name__ == "__main__":
+   main(sys.argv)
